a063e31550daff3a060074e51900edc7fee19f52
M Azhar Alauddin <muhammadazharalauddin@gmail.com>
2025-11-01 23:57:51 +0700

    feat: toast top right fixes

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -10,7 +10,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
-      position="top-left"
+      position="top-right"
       className="toaster group"
       toastOptions={{
         classNames: {
